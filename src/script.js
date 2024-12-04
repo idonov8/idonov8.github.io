6fee547b6cb15220e836882cc20eb7cb65d7ffc2
Ido Nov <idonov8@gmail.com>
2024-12-04 16:52:54 +0900

    add projects dropdown and background texture

--- src/script.js
+++ src/script.js
@@ -5,11 +5,11 @@ $( () => {
 
         data.sections.forEach((section, index) => {
             const $sectionDiv = $('<div>', { class: 'links-col' });
-            const $header = $('<h1>')
-			if (section.icon) {
-				$header.append(`<img src="assets/${section.icon}" />`)
-			}
-			$header.append(section.title);
+            const $header = $('<h1>');
+            if (section.icon) {
+                $header.append(`<img src="assets/${section.icon}" />`);
+            }
+            $header.append(section.title);
             $sectionDiv.append($header);
 
             section.links.forEach(link => {
@@ -21,6 +21,26 @@ $( () => {
                 $sectionDiv.append($anchor);
             });
 
+            // Add projects dropdown
+            if (section.projects && section.projects.length > 0) {
+                const $dropdown = $('<div>', { class: 'projects' });
+                const $dropdownButton = $('<div>', { class: 'projects-button', text: 'Projects', tabIndex: 0 });
+                $dropdownButton.append(`<i class="arrow down"></i>`)
+                const $dropdownContent = $('<div>', { class: 'projects-content' });
+                
+                section.projects.forEach(project => {
+                    const $projectLink = $('<a>', {
+                        href: project.url,
+                        target: '_blank',
+                        text: project.title
+                    });
+                    $dropdownContent.append($projectLink);
+                });
+
+                $dropdown.append($dropdownButton);
+                $dropdown.append($dropdownContent);
+                $sectionDiv.append($dropdown);
+            }
             $linksSection.append($sectionDiv);
 
             if (index < data.sections.length - 1) {
@@ -31,14 +51,23 @@ $( () => {
     }).fail(function(error) {
         console.error(error);
     }).then(function() {
-		$('a').click(function(e) {
-			e.preventDefault();
-			var linkUrl = $(this).attr('href');
-			setTimeout(function(url) { 
-				window.location = url; 
-				window.target = "_blank";
-				$('a').trigger('blur');
-			}, 500, linkUrl);
-		  });
-	});
+        $('a').click(function(e) {
+            e.preventDefault();
+            var linkUrl = $(this).attr('href');
+            setTimeout(function(url) {
+                window.location = url;
+                window.target = "_blank";
+                $('a').trigger('blur');
+            }, 500, linkUrl);
+        });
+
+        $('.projects-button').on('click', function() {
+            $(this).toggleClass('clicked')
+            $(this).find('.arrow').toggleClass('down up');
+            $(this).parent().find('.projects-content').toggle();
+            if (!$(this).hasClass('clicked')) {
+                $(this).blur();
+            }
+        });
+    });
 });
