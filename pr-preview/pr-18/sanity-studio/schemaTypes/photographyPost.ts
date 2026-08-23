8fe15a2fda036c68235583da461efd0ce08a389f
idonov8 <idonov8@users.noreply.github.com>
2026-08-23 15:45:58 +0000

    Deploy preview for PR 18 🛫

---
+++ pr-preview/pr-18/sanity-studio/schemaTypes/photographyPost.ts
@@ -0,0 +1,34 @@
+import {defineType, defineField, defineArrayMember} from 'sanity'
+import {ImageIcon} from '@sanity/icons'
+
+export const photographyPost = defineType({
+  name: 'photographyPost',
+  title: 'Photography Post',
+  type: 'document',
+  icon: ImageIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [defineArrayMember({type: 'image', options: {hotspot: true}})],
+    }),
+  ],
+  orderings: [
+    {
+      title: 'Date, New',
+      name: 'dateDesc',
+      by: [{field: 'date', direction: 'desc'}],
+    },
+  ],
+})
