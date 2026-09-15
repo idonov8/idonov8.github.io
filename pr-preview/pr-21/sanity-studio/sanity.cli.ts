0f69bbd5274bdd59ad6c6133eae649a9ac5b033b
idonov8 <idonov8@users.noreply.github.com>
2026-09-15 13:11:20 +0000

    Deploy preview for PR 21 🛫

---
+++ pr-preview/pr-21/sanity-studio/sanity.cli.ts
@@ -0,0 +1,18 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'skutmuxd',
+    dataset: 'photography'
+  },
+  project: {
+    basePath: '/admin'
+  },
+  deployment: {
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+     */
+    autoUpdates: true,
+  }
+})
